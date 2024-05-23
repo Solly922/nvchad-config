da0b3dc55afdaa74deb7d915dcb2646c5a25d13d
Solomon Schuster <solomon.schuster01@gmail.com>
2024-05-22 17:10:34 -0700

    adds prettier eslint to ensure installed

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,8 +20,8 @@ return {
   	opts = {
   		ensure_installed = {
   			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier", 
-        "typescript-language-server"
+  			"html-lsp", "css-lsp" , "prettier", "prettier_d" 
+        "typescript-language-server", "eslint_d"
   		},
   	},
   },
