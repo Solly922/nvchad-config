e2d7ef7c1e01ea52948e072e55a0ddb07c1c6950
Solomon Schuster <solomon.schuster01@gmail.com>
2024-05-30 15:57:07 -0700

    changes style

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,7 +7,8 @@ local M = {}
 M.ui = {
   theme = "catppuccin",
   statusline = {
-    theme = "default",
+    theme = "minimal",
+    separator_style = "round",
   },
   nvdash = {
     load_on_startup = true,
