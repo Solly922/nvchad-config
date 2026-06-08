d2d624fbddde7e8bbff4214bcfabf16769164f2f
Solomon Schuster <solomon.schuster01@gmail.com>
2026-06-08 10:54:57 -0700

    updates

--- lua/configs/zbirenbaum-copilot.lua
+++ lua/configs/zbirenbaum-copilot.lua
@@ -6,7 +6,7 @@ ZCopilot.setup = function()
     return
   end
   copilot.setup {
-    copilot_model = "gpt-5.1-codex",
+    copilot_model = "",
     panel = {
       enabled = true,
       auto_refresh = true,
