04cc6b64b3a61b19a90fe037d93612327222eafb
Solomon Schuster <solomon.schuster01@gmail.com>
2026-09-17 10:27:59 -0700

    autoindent

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,9 @@ o.linebreak = true
 
 local opt = vim.opt
 opt.relativenumber = true
+
+-- Keep new lines indented inside blocks (functions, ifs, ...).
+-- NvChad enables smartindent (C-braces only); autoindent is the fallback
+-- that copies the previous line's indent, while treesitter/filetype indent
+-- adds a level where the language supports it.
+opt.autoindent = true
